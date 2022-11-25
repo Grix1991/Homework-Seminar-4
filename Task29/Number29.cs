44717610760953c401a09921f11ddad354ffc4c7
Grisha <micronesia@mail.ru>
2022-11-25 17:59:16 +0300

    Задача №29

---
+++ Task29/Number29.cs
@@ -0,0 +1,10 @@
+﻿/*Задача 29: Напишите методы, 
+который задаёт массив из 8 элементов 
+и выводит их на экран.*/
+
+using static Library;
+int[] col = CreateArray(8);
+Console.WriteLine(PrintGood(col));
+Console.WriteLine();
+Fill(col, 1, 9);
+Console.WriteLine(PrintGood(col));
